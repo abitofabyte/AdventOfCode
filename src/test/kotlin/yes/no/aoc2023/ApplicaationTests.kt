54a0efeac312629e86d924bb2837c6daabd393c3
ABitOfAByte <marcus.migotti@gmail.com>
2023-12-05 16:24:04 +0100

    initial commit

---
+++ src/test/kotlin/yes/no/aoc2023/ApplicaationTests.kt
@@ -0,0 +1,13 @@
+package yes.no.aoc2023
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicaationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
